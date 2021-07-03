076b43e3bc02b6dbb3190d3fe44f8fe5b3f33595
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2021-07-03 12:53:51 +0530

    init

---
+++ Fiber landing page/script/script.js
@@ -0,0 +1,10 @@
+
+// Nav btn
+const ham = document.getElementById('ham');
+const menu = document.getElementById('opt');
+
+ham.addEventListener("click", function(){ 
+    menu.classList.toggle('height');
+ });
+
+ 
