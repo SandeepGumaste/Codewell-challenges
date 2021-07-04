7adbba8c4820348ff36bdc343753877528287cbd
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2021-07-04 14:52:47 +0530

    js

--- Fiber landing page/script/script.js
+++ Fiber landing page/script/script.js
@@ -4,8 +4,6 @@ const ham = document.getElementById('ham');
 const menu = document.getElementById('opt');
 
 ham.addEventListener("click", function(){ 
-    menu.classList.toggle('height');
-    menu.classList.toggle('animate__animated');
-    menu.classList.toggle('animate__fadeInDown');
+    menu.classList.toggle('height', 'animate__animated', 'animate__fadeInDown');
  });
 
